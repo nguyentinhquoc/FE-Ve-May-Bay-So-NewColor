f9006aabb3a270c1f15c95dd099f2269154c3dde
nguyentinhquoc <nguyentinh140321@gmail.com>
2025-10-28 00:00:24 +0700

    ok

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  base: process.env.NODE_ENV === "production" ? "./" : "./",
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        flightList: path.resolve(__dirname, "src/Flight-List.html"),
+        flightInfo: path.resolve(__dirname, "src/Flight-Info.html"),
+        flightPayment: path.resolve(__dirname, "src/Flight-Payment.html"),
+        flight: path.resolve(__dirname, "src/flight.html"),
+        // ...
+      },
+    },
+  },
+  plugins: [ViteEjsPlugin()],
+});

696e5b523843446c1bec1196375b2a450c20b661
wavebear <wavebeardev@gmail.com>
2025-10-30 18:30:00 +0700

    update new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ejs}",
-  ],
-}
-
+export default {
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: "40rem",   // 640px
+        md: "48rem",   // 768px
+        lg: "64rem",   // 1024px
+        xl: "80rem",   // 1280px
+        // ❌ không khai báo 2xl để bỏ rule 96rem
+      },
+    },
+  },
+};

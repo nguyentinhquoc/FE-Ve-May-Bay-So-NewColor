fa0b293458df8c8b4773d9ebe1c4067f529cbef4
wavebear <wavebeardev@gmail.com>
2025-10-30 22:28:46 +0700

    update desktop first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  content: [
+    "./src/**/*.{html,ejs,js}"
+  ],
   theme: {
+    // Đặt breakpoint desktop-first ở đây
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
+    // Chỉ control .container ở đây
     container: {
       center: true,
+      padding: "1rem",
       screens: {
-        sm: "40rem",   // 640px
-        md: "48rem",   // 768px
-        lg: "64rem",   // 1024px
-        xl: "80rem",   // 1280px
-        // ❌ không khai báo 2xl để bỏ rule 96rem
-      },
+        sm: "40rem",
+        xl: "48rem",
+        lg: "64rem",
+        xl: "80rem",
+        "2xl": "80rem"
+      }
     },
+    extend: {},
   },
-};
+  plugins: [],
+}
